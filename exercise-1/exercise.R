bb8c5f468892f0c4eb8bdbf36ac990e06c12eb25
Jisoo Kim <jisoo96@uw.edu>
2017-11-14 13:58:17 -0800

    fixed

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,9 +1,16 @@
 # Exercise 1: Loading functions
 
 # Set your directory
-
+setwd("~/Desktop/INFO201/ch16-shiny/exercise-1/")
 
 # Source your BuildScatter.r script, exposing your BuildScatter function
-
+source("./scripts/BuildScatter.R")
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
+BuildScatter(data = iris, 
+             xVar = 'Sepal.Length', 
+             yVar = 'Sepal.Width',
+             colorVar = 'Species', 
+             title = 'Iris Dataset', 
+             xLab = 'Sepal Length', 
+             yLab = 'Sepal Width')
