e6f0e5bcc24cc4fbad53124e0e5b44aa7a1823cf
Jisoo Kim <jisoo96@uw.edu>
2017-11-14 14:02:33 -0800

    fixed

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,10 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+BuildScatter <- function(data, x.var, y.var, color.var, title = "Title", x.label = "X Title", y.label = "Y Title"){
+  plot <- ggplot2(data = data) +
+          geom_point(mapping = aes(x = data[, x.var], y = data[, y.var], color = data[,color.var])) +
+          labs(title = title, x = x.label, y = y.label)
+  return(plot)
+}
